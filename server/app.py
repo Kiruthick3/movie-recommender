8a31206759bdb9521d9d7e1d1c0edb759c19ecd3
Kiruthick3 <kiruthickofficial@gmail.com>
2025-09-09 21:00:32 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+from core import nltk_config
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 from core.config import settings
@@ -6,22 +8,6 @@ from core.db import mongo
 from auth.routes import auth_bp
 from movies.routes import movies_bp
 from users.routes import users_bp
-
-import os
-import nltk
-
-nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
-nltk.data.path.insert(0, nltk_data_path)
-
-try:
-    nltk.corpus.wordnet.ensure_loaded()
-    nltk.data.find('sentiment/vader_lexicon')
-except LookupError:
-    raise RuntimeError(
-        "NLTK corpora not found in server/nltk_data folder. "
-        "Make sure 'wordnet' and 'vader_lexicon' are present."
-    )
-
 
 def create_app():
     app = Flask(__name__)
