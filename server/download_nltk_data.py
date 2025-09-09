6de14beed8efbd8776585d873edd9364a6e41c57
Kiruthick3 <kiruthickofficial@gmail.com>
2025-09-09 18:47:02 +0530

    Update download_nltk_data.py

--- server/download_nltk_data.py
+++ server/download_nltk_data.py
@@ -1,4 +1,15 @@
 import nltk
+import os
 
-nltk.download('vader_lexicon')
-nltk.download('wordnet')
+nltk_data_path = os.path.join(os.path.dirname(__file__), '../nltk_data')
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.corpus.wordnet.ensure_loaded()
+except LookupError:
+    nltk.download('wordnet', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('sentiment/vader_lexicon')
+except LookupError:
+    nltk.download('vader_lexicon', download_dir=nltk_data_path)
